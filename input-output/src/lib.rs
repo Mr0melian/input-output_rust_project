390c9841e821da62c137470e7646fde4cf99f671
Ivan <serhieievivan6@gmail.com>
2023-03-30 21:13:06 +0300

    pre realise commit

--- input-output/src/lib.rs
+++ input-output/src/lib.rs
@@ -16,9 +16,21 @@ impl Config{
 }
 
 pub fn run (config: Config)->Result<(),Box<dyn Error>>{
-    let contest = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contest);
+    let contents = fs::read_to_string(config.filename)?;
+    for line in search(&config.query, &contents){
+        println!("{}", line);
+    }
     Ok(())
+}
+ 
+pub fn search<'a>(query: &str, contents: &'a str)-> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query){
+            result.push(line);
+        }
+    }
+    result
 }
 
 
@@ -45,5 +57,21 @@ mod tests {
         if let Err(e) = run(config) {
             assert!(true)
         }
+    }
+
+   
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
     }
 }
---
+++ input-output/target/debug/libinput_output.d
@@ -0,0 +1,1 @@
+/home/ivan/input-output_rust_project/input-output/target/debug/libinput_output.rlib: /home/ivan/input-output_rust_project/input-output/src/lib.rs
